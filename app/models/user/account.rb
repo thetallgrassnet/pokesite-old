c91c2de56b5b958887c116183f08be596690bc32
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-07 20:16:10 -0700

    Validate account name format
    Only allow word characters (letters, numbers, and underscores)

--- app/models/user/account.rb
+++ app/models/user/account.rb
@@ -2,7 +2,10 @@ class User::Account < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, format: {
+    with: %r{\A\w+\Z},
+    message: "may only contain letters (A-Z, a-z), numbers (0-9), and underscores (_)"
+  }
 
   def to_param
     name

0a465d9e42209dbf72b6a3783173496e1ea90efd
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-07 21:37:06 -0700

    Fuzzy obscenity validator documentation/credits.

--- lib/fuzzy_obscenity_validator.rb
+++ lib/fuzzy_obscenity_validator.rb
@@ -1,3 +1,10 @@
+# Case-insensitively and fuzzily validates a string value for obscenity against
+# a blacklist of foul language.
+#
+# Based on Obscenity::Base and Obscenity::ActiveModel from the Obscenity gem.
+#
+# https://github.com/tjackiw/obscenity/blob/master/lib/obscenity/base.rb
+# https://github.com/tjackiw/obscenity/blob/master/lib/obscenity/active_model.rb
 class FuzzyObscenityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, options[:message] || 'cannot be profane') if profane?(value)

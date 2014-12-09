bd692109a590e45e9a0154fdd513167c20eec7ee
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-08 22:44:36 -0700

    Smaller centered Devise forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   include FoundationRailsHelper::FlashHelper
 
+  def controller_classes
+    controller_path.gsub '/', ' '
+  end
+
   def join_title(*args)
     args.join(" | ")
   end

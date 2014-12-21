b174329bbbadc1bd7e8088c8006f60ff17155187
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-20 23:49:29 -0700

    User::Account factory

--- spec/factories/user_accounts.rb
+++ spec/factories/user_accounts.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user_account, :class => 'User::Account' do
-    
+    sequence(:username) { |n| "user#{n}" }
+    email { "#{username}@domain.com" }
+    password "P@55w0rd"
+    password_confirmation "P@55w0rd"
   end
 
 end

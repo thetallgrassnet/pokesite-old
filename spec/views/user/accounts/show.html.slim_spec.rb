0ee626c8ac24bee58c8c355b59003ee39e1f70ce
Jesse B. Hannah <jesse@jbhannah.net>
2015-01-22 23:24:30 -0700

    user/accounts/show view spec

--- spec/views/user/accounts/show.html.slim_spec.rb
+++ spec/views/user/accounts/show.html.slim_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "accounts/show.html.slim", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "user/accounts/show.html.slim", type: :view do
+  let(:date) { DateTime.now }
+  let(:account) { FactoryGirl.build(:user_account, created_at: date) }
+
+  it "displays the specified user" do
+    assign(:account, account)
+
+    render
+
+    expect(rendered).to match %r{<h1>#{account.username}</h1>}
+    expect(rendered).to match %r{Joined on #{date.strftime("%B %-d, %Y")}}
+  end
 end

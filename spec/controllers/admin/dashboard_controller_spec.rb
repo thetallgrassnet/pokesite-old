680372ee77140d15c2bdc64af3cee4632228be25
Jesse B. Hannah <jesse@jbhannah.net>
2015-02-14 23:04:47 -0700

    Admin dashboard controller spec

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -3,9 +3,29 @@ require 'rails_helper'
 RSpec.describe Admin::DashboardController, type: :controller do
 
   describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context "authenticated" do
+      login_admin
+
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "unauthenticated" do
+      it "returns http redirect" do
+        get :index
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "unauthorized" do
+      login_user
+
+      it "reutrns http redirect" do
+        get :index
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -5,4 +5,11 @@ module ControllerMacros
       sign_in FactoryGirl.create(:user_account, confirmed_at: DateTime.now)
     end
   end
+
+  def login_admin
+    before(:example) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:admin, confirmed_at: DateTime.now)
+    end
+  end
 end

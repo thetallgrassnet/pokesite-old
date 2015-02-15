a139bb50a1695efeba8b4cb397cb59b12d3000e7
Jesse B. Hannah <jesse@jbhannah.net>
2015-02-15 11:19:13 -0700

    Bootstrap alert flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,25 @@
 module ApplicationHelper
-  include FoundationRailsHelper::FlashHelper
-
   def controller_classes
     controller_path.gsub '/', ' '
   end
 
   def join_title(*args)
     args.join(" | ")
+  end
+
+  def display_flash_messages
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
+        concat content_tag(:button, "&times;".html_safe, type: "button", class: "close", data: { dismiss: "alert" }, "aria-label": t("common.close"))
+        concat message
+      end)
+    end
+    nil
+  end
+
+  private
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 end

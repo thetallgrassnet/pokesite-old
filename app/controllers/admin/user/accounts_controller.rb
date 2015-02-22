8e97be137dd1377a297ee4b01b48736c05c2a8ad
Jesse B. Hannah <jesse@jbhannah.net>
2015-02-22 14:49:05 -0700

    Load user account before non-index actions in admin

--- app/controllers/admin/user/accounts_controller.rb
+++ app/controllers/admin/user/accounts_controller.rb
@@ -1,5 +1,6 @@
 class Admin::User::AccountsController < Admin::BaseController
   before_action { authorize_manage(User::Account) }
+  before_action :load_user_account, except: [:index]
 
   def index
     @accounts = User::Account.all
@@ -15,5 +16,11 @@ class Admin::User::AccountsController < Admin::BaseController
   end
 
   def delete
+  end
+
+  private
+
+  def load_user_account
+    @account = User::Account.find_by uuid: params[:id]
   end
 end

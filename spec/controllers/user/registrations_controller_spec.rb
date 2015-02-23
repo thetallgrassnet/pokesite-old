d9ed850601542e013d3dbe75291efba420f43d10
Jesse B. Hannah <jesse@jbhannah.net>
2015-02-22 17:19:44 -0700

    User::RegistrationsController spec

--- spec/controllers/user/registrations_controller_spec.rb
+++ spec/controllers/user/registrations_controller_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User::RegistrationsController, type: :controller do
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "DELETE destroy" do
+    context "user" do
+      login_user
+
+      it "does not raise Allowy::AccessDenied" do
+        bypass_rescue
+        expect { delete :destroy }.not_to raise_error
+      end
+    end
+
+    context "admin" do
+      login_admin
+
+      it "raises Allowy::AccessDenied" do
+        bypass_rescue
+        expect { delete :destroy }.to raise_error(Allowy::AccessDenied)
+      end
     end
   end
 

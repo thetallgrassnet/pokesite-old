688d46d29bee9999dd2252f7370446f893cfb96a
Jesse B. Hannah <jesse@jbhannah.net>
2015-02-22 21:57:25 -0700

    Admin user accounts index view spec

--- spec/views/admin/user/accounts/index.html.slim_spec.rb
+++ spec/views/admin/user/accounts/index.html.slim_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "accounts/index.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "admin/user/accounts/index.html.slim", type: :view do
+  it "renders the template" do
+    assign(:accounts, 5.times.collect { FactoryGirl.create(:user_account, confirmed_at: DateTime.now) })
+
+    allow(view).to receive(:can?).with(:destroy, kind_of(User::Account)) { true }
+    render
+  end
 end

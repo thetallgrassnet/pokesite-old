f732d7fc12d2375d1a0c5ea01ba47f1142b10e27
Jesse B. Hannah <jesse@jbhannah.net>
2015-06-27 01:10:25 -0700

    Edit/update basic account information in admin site

--- app/controllers/admin/user/accounts_controller.rb
+++ app/controllers/admin/user/accounts_controller.rb
@@ -13,6 +13,17 @@ class Admin::User::AccountsController < Admin::BaseController
   end
 
   def update
+    begin
+      if params[:user_account][:password].blank? && params[:user_account][:password_confirmation].blank?
+        params[:user_account].delete(:password)
+        params[:user_account].delete(:password_confirmation)
+      end
+
+      @account.update_attributes! params[:user_account]
+      redirect_to admin_user_account_path(@account.uuid)
+    rescue Exception => e
+      render :edit
+    end
   end
 
   def delete

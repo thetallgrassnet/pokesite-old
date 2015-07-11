578edc9cd65bfaef6b6e9c446bc2e4f8fff2647b
Jesse B. Hannah <jesse@jbhannah.net>
2015-07-11 08:44:06 -0700

    Default update and destroy access methods

--- app/models/base_access.rb
+++ app/models/base_access.rb
@@ -2,6 +2,14 @@ class BaseAccess
   include Allowy::AccessControl
   delegate :current_user, to: :context
 
+  def update?(klass)
+    manage?(klass)
+  end
+
+  def destroy?(klass)
+    manage?(klass)
+  end
+
   def manage?(klass)
     current_user.is_admin?
   end

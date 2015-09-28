7427eec91897e557dbd97231b2ade51d0e2d9eb6
Jesse B. Hannah <jesse@jbhannah.net>
2015-09-28 16:26:16 -0700

    Make User::Account username factory reusable

--- spec/factories/user_accounts.rb
+++ spec/factories/user_accounts.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
+  sequence(:username) { |n| "user#{n}" }
+
   factory :user_account, class: 'User::Account' do
-    sequence(:username) { |n| "user#{n}" }
+    username
     email { "#{username}@domain.com" }
     password "P@55w0rd"
     password_confirmation "P@55w0rd"

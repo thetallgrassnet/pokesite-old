832d3a6025dd1242513c3bb6e306da3154aa063e
Jesse B. Hannah <jesse@jbhannah.net>
2015-09-28 16:26:38 -0700

    Test Admin::User::AccountsController#edit, #update

--- spec/controllers/admin/user/accounts_controller_spec.rb
+++ spec/controllers/admin/user/accounts_controller_spec.rb
@@ -39,4 +39,27 @@ RSpec.describe Admin::User::AccountsController, type: :controller do
     end
   end
 
+  describe "GET edit" do
+    let(:user) { FactoryGirl.create(:user_account, :confirmed) }
+    login_admin
+
+    it "retrieves the user" do
+      get :edit, id: user.uuid
+      expect(assigns(:user_account)).to eql(user)
+    end
+  end
+
+  describe "PUT update" do
+    let(:user) { FactoryGirl.create(:user_account, :confirmed) }
+    let(:username) { FactoryGirl.generate(:username) }
+    login_admin
+
+    it "updates the user" do
+      put :update, id: user.uuid, user_account: { username: username }
+      user.reload
+      expect(response).to have_http_status(:redirect)
+      expect(user.username).to eql(username)
+    end
+  end
+
 end

6102988ef361f854e14bf61f02321c497e39f69f
Jesse B. Hannah <jesse@jbhannah.net>
2015-09-29 08:47:49 -0700

    Delete unused BaseAccess#update?

--- app/models/base_access.rb
+++ app/models/base_access.rb
@@ -2,10 +2,6 @@ class BaseAccess
   include Allowy::AccessControl
   delegate :current_user, to: :context
 
-  def update?(klass)
-    manage?(klass)
-  end
-
   def destroy?(klass)
     manage?(klass)
   end

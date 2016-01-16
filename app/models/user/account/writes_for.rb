3759b0c49766125fb96528b54f1468bb33b50a9e
Jesse B. Hannah <jesse@jbhannah.net>
2016-01-15 21:23:24 -0700

    from/to_class must be a symbol, string, false, or nil
    or an array of symbols and strings

--- app/models/user/account/writes_for.rb
+++ app/models/user/account/writes_for.rb
@@ -1,8 +1,8 @@
 class User::Account::WritesFor
   include Neo4j::ActiveRel
 
-  from_class User::Account
-  to_class Article::Column
+  from_class :"User::Account"
+  to_class :"Article::Column"
   type :writes_for
 
   property :is_editor, type: Boolean, null: false, default: false

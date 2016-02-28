99abddb8b8ccfd8b6e73f17c98515ffc4f7cf063
Jesse B. Hannah <jesse@jbhannah.net>
2016-02-28 13:49:50 -0700

    Load and link to all columns in header nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :load_columns
 
   rescue_from Allowy::AccessDenied do |e|
     redirect_to root_url, flash: { error: t("common.not_authorized") }
@@ -23,5 +24,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:account_update) << :username
+  end
+
+  def load_columns
+    @columns = Article::Column.all
   end
 end

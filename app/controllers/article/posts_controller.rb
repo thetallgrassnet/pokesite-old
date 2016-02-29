5a98cc629edc84185fd54da24934f19039494af5
Jesse B. Hannah <jesse@jbhannah.net>
2016-02-28 20:24:09 -0700

    Limit query to one article post

--- app/controllers/article/posts_controller.rb
+++ app/controllers/article/posts_controller.rb
@@ -1,7 +1,7 @@
 class Article::PostsController < ApplicationController
   def show
     @post = Article::Post.all(:p).column(:c)
-      .where('p.slug = {p_slug} and c.slug = {c_slug}')
+      .where('p.slug = {p_slug} and c.slug = {c_slug}').limit(1)
       .params(p_slug: params[:id], c_slug: params[:column_id]).pluck(:p).first
     authorize! :show, @post
   end

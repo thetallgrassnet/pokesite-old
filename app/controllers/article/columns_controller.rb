e70767e6e6c0e2ac1e0db069c0b63486070cd858
Jesse B. Hannah <jesse@jbhannah.net>
2016-02-28 20:26:13 -0700

    Use published scope for retrieving posts

--- app/controllers/article/columns_controller.rb
+++ app/controllers/article/columns_controller.rb
@@ -1,14 +1,14 @@
 class Article::ColumnsController < ApplicationController
   def index
     @title = "Articles"
-    @posts = Article::Post.all
+    @posts = Article::Post.published
 
     render "article/posts/index"
   end
 
   def show
     @column = Article::Column.find_by! slug: params[:id]
-    @posts = @column.posts
+    @posts = @column.posts.published
     @title = @column.name
 
     render "article/posts/index"

0bc46f8521fc55ba5a0074c5a6901412e2260b5d
Jesse B. Hannah <jesse@jbhannah.net>
2016-02-28 21:22:27 -0700

    Fetch posts for welcome controller and order by published_at descending

--- app/controllers/article/columns_controller.rb
+++ app/controllers/article/columns_controller.rb
@@ -1,14 +1,14 @@
 class Article::ColumnsController < ApplicationController
   def index
     @title = "Articles"
-    @posts = Article::Post.published
+    @posts = Article::Post.published.order(published_at: :desc)
 
     render "article/posts/index"
   end
 
   def show
     @column = Article::Column.find_by! slug: params[:id]
-    @posts = @column.posts.published
+    @posts = @column.posts.published.order(published_at: :desc)
     @title = @column.name
 
     render "article/posts/index"

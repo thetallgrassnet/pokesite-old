e2006ee0067ee0c3710d633363c4418df712b3b0
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-03 00:06:58 -0700

    About and privacy action specs

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe "GET index" do
     subject { get :index }
-
     it { is_expected.to render_template :index }
+  end
+
+  describe "GET about" do
+    subject { get :about }
+    it { is_expected.to render_template :about }
+  end
+
+  describe "GET privacy" do
+    subject { get :privacy }
+    it { is_expected.to render_template :privacy }
   end
 
 end

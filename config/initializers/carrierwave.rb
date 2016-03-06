03562c9345c9d499d1c3fc53b786f1aad3ee81a3
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-06 12:28:58 -0700

    Fix CarrierWave cache directory for Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],

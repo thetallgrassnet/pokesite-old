93d3514ef606ab517fe6ae03066641d7c0f169c8
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-11 23:43:52 -0700

    Use Namekuji for slug generation in article posts and columns

--- app/models/article/column.rb
+++ app/models/article/column.rb
@@ -2,6 +2,8 @@ class Article::Column
   include Neo4j::ActiveNode
   include Neo4j::Timestamps
 
+  include Namekuji
+
   property :name, type: String, null: false, default: "", constraint: :unique
   property :slug, type: String, null: false, default: "", constraint: :unique
 
@@ -14,26 +16,12 @@ class Article::Column
   has_many :in, :posts, origin: :column, model_class: :"Article::Post"
 
   validates :name, presence: true, uniqueness: true
-  validates :slug, presence: true, uniqueness: true, format: {
-    with: %r{\A[0-9a-z-]+\Z},
-    message: "may only contain lowercase letters (a-z), numbers (0-9), and dashes (-)"
-  }
 
   validates :description, presence: true
 
-  before_validation :slugify
-
-  def to_param
-    slug
-  end
+  sluggable on_field: :name
 
   def to_s
     name
-  end
-
-  private
-
-  def slugify
-    self.slug = name.parameterize
   end
 end

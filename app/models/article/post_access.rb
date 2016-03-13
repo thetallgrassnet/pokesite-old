c219b4f886b031286826c2e4b56451007db25851
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-12 20:04:00 -0700

    Article::PostAccess#manage? allows admins and authors

--- app/models/article/post_access.rb
+++ app/models/article/post_access.rb
@@ -1,4 +1,8 @@
 class Article::PostAccess < BaseAccess
+  def manage?(post)
+    current_user.is_admin? or current_user.columns.size > 0
+  end
+
   def show?(post)
     post.published?
   end
--- spec/models/article/post_access_spec.rb
+++ spec/models/article/post_access_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 RSpec.describe Article::PostAccess do
   let(:access) { Article::PostAccess.new double(current_user: FactoryGirl.build(:user_account)) }
 
+  context "an admin user" do
+    let(:admin_access) { Article::PostAccess.new double(current_user: FactoryGirl.build(:user_account, :is_admin)) }
+
+    describe "#manage?" do
+      subject { admin_access.manage? Article::Post }
+      it { is_expected.to be true }
+    end
+  end
+
+  context "an author" do
+    let(:author_access) { Article::PostAccess.new double(current_user: FactoryGirl.create(:author)) }
+
+    describe "#manage?" do
+      subject { author_access.manage? Article::Post }
+      it { is_expected.to be true }
+    end
+  end
+
   context "a published post" do
     let(:post) { FactoryGirl.create(:article_post) }
 

9c7f7aeef42a188a5d088e3a2fe0bc1909026d28
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-13 15:38:42 -0700

    On article posts admin index, show all posts if admin
    Otherwise show all posts from current user's columns

--- app/controllers/admin/article/posts_controller.rb
+++ app/controllers/admin/article/posts_controller.rb
@@ -1,3 +1,9 @@
 class Admin::Article::PostsController < Admin::ResourceController
   resource ::Article::Post, %w( headline subhead body is_featured published_at feature_image )
+
+  def index
+    (redirect_to(action: :index) and return) if params[:page] == "1"
+    @article_posts = (current_user.is_admin? ? ::Article::Post.all : current_user.columns.posts)
+      .order(created_at: :desc).page(params[:page]).per(10)
+  end
 end

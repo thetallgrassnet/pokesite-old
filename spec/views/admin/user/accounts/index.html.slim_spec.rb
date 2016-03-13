f59f50ebd72ca4e4bce9dc0fe7ba6f02d8bb75f6
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-12 22:41:48 -0700

    Fix user accounts admin index spec for Kaminari

--- spec/views/admin/user/accounts/index.html.slim_spec.rb
+++ spec/views/admin/user/accounts/index.html.slim_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "admin/user/accounts/index.html.slim", type: :view do
+  let(:accounts) { FactoryGirl.create_list(:user_account, 5) }
   it "renders the template" do
-    assign(:user_accounts, 5.times.collect { FactoryGirl.create(:user_account, confirmed_at: DateTime.now) })
+    assign(:user_accounts, Kaminari.paginate_array(accounts).page(1))
 
     allow(view).to receive(:can?).with(:destroy, kind_of(User::Account)) { true }
     render

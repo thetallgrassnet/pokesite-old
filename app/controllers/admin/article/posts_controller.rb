950d5815befdc085b258333aa8a1674f1dc8aa91
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-18 23:51:18 -0700

    Admin article post form

--- app/controllers/admin/article/posts_controller.rb
+++ app/controllers/admin/article/posts_controller.rb
@@ -1,9 +1,15 @@
 class Admin::Article::PostsController < Admin::ResourceController
-  resource ::Article::Post, %w( headline subhead body is_featured published_at feature_image )
+  resource ::Article::Post, %w( author_id column_id headline subhead body is_featured published_at feature_image )
 
   def index
     (redirect_to(action: :index) and return) if params[:page] == "1"
     @article_posts = (current_user.is_admin? ? ::Article::Post.all : current_user.columns.posts)
       .order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def create
+    params[:article_post][:author_id] = current_user.uuid
+    params[:article_post][:published_at] = nil if params[:article_post][:published_at].empty?
+    super
   end
 end

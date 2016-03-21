581a1a0adf88a0aac6e274371fcd4c549c818551
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-20 22:32:09 -0700

    Explicitly cast Article::Post#published_at
    Neo4j.rb 6.x used ActiveAttr for typecasting, which used ActiveSupport's
    String#to_datetime extension. Neo4j.rb 7.x removed all dependencies on
    ActiveAttr and instead uses DateTime.strptime with an explicit format
    for conversion, which doesn't work by default with generated Rails form
    fields.

--- app/controllers/admin/article/posts_controller.rb
+++ app/controllers/admin/article/posts_controller.rb
@@ -1,6 +1,8 @@
 class Admin::Article::PostsController < Admin::ResourceController
   resource ::Article::Post, %w( author_id column_id headline subhead body is_featured published_at feature_image )
 
+  before_filter :typecast_published_at, only: [:create, :update]
+
   def index
     (redirect_to(action: :index) and return) if params[:page] == "1"
     @article_posts = (current_user.is_admin? ? ::Article::Post.all : current_user.columns.posts)
@@ -9,7 +11,13 @@ class Admin::Article::PostsController < Admin::ResourceController
 
   def create
     params[:article_post][:author_id] = current_user.uuid
-    params[:article_post][:published_at] = nil if params[:article_post][:published_at].empty?
     super
+  end
+
+  private
+
+  def typecast_published_at
+    params[:article_post][:published_at] = params[:article_post][:published_at].to_datetime \
+      if params[:article_post][:published_at].respond_to? :to_datetime
   end
 end

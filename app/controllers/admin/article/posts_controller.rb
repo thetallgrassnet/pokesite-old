b7d1735ba75f134247c5a0d60abdaff742b1b6d7
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-21 19:57:24 -0700

    Revert "Explicitly cast Article::Post#published_at"
    581a1a0adf88a0aac6e274371fcd4c549c818551
    
    Neo4j.rb 7.0.1 goes back to using to_datetime for typecasting strings to
    DateTime objects.

--- app/controllers/admin/article/posts_controller.rb
+++ app/controllers/admin/article/posts_controller.rb
@@ -1,8 +1,6 @@
 class Admin::Article::PostsController < Admin::ResourceController
   resource ::Article::Post, %w( author_id column_id headline subhead body is_featured is_published published_at feature_image )
 
-  before_filter :typecast_published_at, only: [:create, :update]
-
   def index
     (redirect_to(action: :index) and return) if params[:page] == "1"
     @article_posts = (current_user.is_admin? ? ::Article::Post.all : current_user.columns.posts)
@@ -11,13 +9,7 @@ class Admin::Article::PostsController < Admin::ResourceController
 
   def create
     params[:article_post][:author_id] = current_user.uuid
+    params[:article_post][:published_at] = nil if params[:article_post][:published_at].empty?
     super
-  end
-
-  private
-
-  def typecast_published_at
-    params[:article_post][:published_at] = params[:article_post][:published_at].to_datetime \
-      if params[:article_post][:published_at].respond_to? :to_datetime
   end
 end
